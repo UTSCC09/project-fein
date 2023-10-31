370e1bf3d696cb1d815e06984eb39a3e2c0f5774
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2023-10-31 02:24:14 -0400

    Initialize tailwind and home page

--- fein/src/App.js
+++ fein/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Home } from './Pages/Homepage';
+
 function App() {
   return (
     <div className="App">
-      Fein
+      
+
+      <Home />
+
+
     </div>
   );
 }
