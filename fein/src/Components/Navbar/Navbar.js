29977c2b6e3faa3580a3f4a4ea7df2b035429ae7
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2023-11-01 10:54:00 -0400

    Add navbar icon animations

--- fein/src/Components/Navbar/Navbar.js
+++ fein/src/Components/Navbar/Navbar.js
@@ -7,12 +7,12 @@ export function Navbar() {
         <nav className="flex bg-navbar sticky top-0">
             <h1 class="nav_logo"> FEIN </h1>
             <div className="flex px-4 py-2 w-full justify-start text">
-                <div class="navbar_element"><MagnifyingGlassIcon class="h-6 w-6 text-white" /></div>
+                <div class="navbar_element"><MagnifyingGlassIcon class="navbar_icon" /></div>
                 <input class="search_bar" type="text" placeholder="Search" />
             </div>
             <div className="flex px-4 py-2 w-full justify-end">
-                <div class="navbar_element"><HomeIcon className="h-7 w-7 text-white" /></div>
-                <div class="navbar_element"><UserIcon className="h-7 w-7 text-white"/></div>
+                <div class="navbar_element"><HomeIcon className="navbar_icon" /></div>
+                <div class="navbar_element"><UserIcon className="navbar_icon"/></div>
 
             </div>
         </nav>
