a932e2a0e35ea94bab823c66679ce82f05f12504
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2023-11-01 02:14:15 -0400

    Add search bar and home icon, as well as icon libraries

--- fein/src/Components/Navbar/Navbar.js
+++ fein/src/Components/Navbar/Navbar.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Navbar.css';
-
+import { HomeIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+ 
 export function Navbar() {
     return (
         <nav className="flex bg-navbar sticky top-0">
             <h1 class="nav_logo"> FEIN </h1>
-            <div className="flex  px-4 py-6 w-full justify-end">
-                    <div class="navbar_element">Home</div>
-                    <div class="navbar_element">About</div>
-                    <div class="navbar_element">Contact</div>
+            <div className="flex px-4 py-6 w-full justify-start">
+                <div class="navbar_element"><MagnifyingGlassIcon class="h-6 w-6 text-white" /></div>
+                <input class="search_bar" type="text" placeholder="Search" />
+            </div>
+            <div className="flex px-4 py-6 w-full justify-end">
+                <div class="navbar_element"><HomeIcon class="h-6 w-6 text-white" /></div>
+                <div class="navbar_element">About</div>
+                <div class="navbar_element">Contact</div>
             </div>
         </nav>
     );
--- fein/src/Pages/Home/Homepage.js
+++ fein/src/Pages/Home/Homepage.js
@@ -2,6 +2,7 @@ import "./Homepage.css"
 
 import React, { useEffect, useState} from "react";
 import { Link } from "react-router-dom";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
 
 // Component imports
 
