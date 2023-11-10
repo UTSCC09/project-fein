0ea1532ba825772b2d367e421735416769251226
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2023-11-10 09:12:28 -0500

    Fix search results

--- fein/client/src/Components/Common/SearchResults.js
+++ fein/client/src/Components/Common/SearchResults.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SearchResults = ({ results }) => {
     return(
-        <ul className="absolute border-gray-300 border-2 w-full rounded-md w-1/6 max-h-40 overflow-y-scroll bg-white border-neutral-200 mt-16">
+        <ul className="absolute border-gray-300 border-2 w-full rounded-md w-1/5 max-h-40 overflow-y-scroll bg-white border-neutral-200 mt-16">
             {results.map((result) => (
-                <li key={result.ticker} className="m-2 flex items-center hover:bg-gray-300 cursor-pointer justify-between">
+                <li key={result.ticker} className="m-2 flex items-center hover:bg-gray-300 cursor-pointer justify-between text-black">
                     <span>{result.name}</span>
                     <span>{result.ticker}</span>
                 </li>
