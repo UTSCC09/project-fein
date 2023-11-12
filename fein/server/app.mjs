ce533954fe2a03e9c885857f66f96a49028a1a88
Avik Chakraborty <92612550+AvikChakraborty1@users.noreply.github.com>
2023-11-12 17:44:22 -0500

    Add more endpoints

--- fein/server/app.mjs
+++ fein/server/app.mjs
@@ -7,23 +7,20 @@ import session from "express-session";
 import { parse, serialize } from "cookie";
 import { genSalt, hash, compare } from "bcrypt";
 import validator from "validator";
+import { url } from "inspector";
 
 const PORT = 4000;
 const app = express();
 
 app.use(express.json());
 
-// const items = new Datastore({
-//   filename: join("db", "items.db"),
-//   autoload: true,
-//   timestampData: true,
-// });
-
 const mongo_client = new MongoClient('mongodb+srv://fein-db-username:!Fein247pp@cluster0.b75qkv4.mongodb.net/?retryWrites=true&w=majority');
 const database = mongo_client.db('Fein');
 
 const users = database.collection('users');
 const positions = database.collection('positions');
+
+const base_path = "https://finnhub.io/api/v1"
 
 // function getItems(page, limit) {
 //     limit = Math.max(5, (limit) ? parseInt(limit) : 5);
@@ -64,18 +61,32 @@ const checkUsername = function (req, res, next) {
     next();
 };
 
+const isAuthenticated = function (req, res, next) {
+    if (!req.session.user._id) return res.status(401).end("access denied");
+    next();
+};
+
 app.use(
     session({
         secret: "Yarrggghh",
         resave: false,
         saveUninitialized: true,
-        cookie: {
-            httpOnly: true, // prevent the session cookie from being read by Javascript onn the browser
-            secure: false,  // prevent the cookie to be sent with http, should be set to true when https is enabled
-            // samesite: 'strict' // prevent the cookie from being sent with cross-domain requests, should be set to lax when frontend is served on different domain
-        }
+        // cookie: {
+        //     // httpOnly: true, // prevent the session cookie from being read by Javascript onn the browser
+        //     // secure: false,  // prevent the cookie to be sent with http, should be set to true when https is enabled
+        //     // samesite: 'strict' // prevent the cookie from being sent with cross-domain requests, should be set to lax when frontend is served on different domain
+        // }
     })
 );
+
+app.use(function (req, res, next) {
+    const username = (req.session.user) ? req.session.user._id : '';
+    res.setHeader('Set-Cookie', serialize('username', username, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7 // 1 week in number of seconds
+    }));
+    next();
+});
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", process.env.FRONTEND);
@@ -103,7 +114,7 @@ app.post("/api/signup/", checkUsername, function (req, res, next) {
                 // insert new user into the database
                 users.updateOne(
                     { _id: username },
-                    { _id: username, hash: hash },
+                    { _id: username, hash: hash, fein_bucks: 3000 },
                     { upsert: true },
                     function (err) {
                         if (err) return res.status(500).end(err);
@@ -115,24 +126,95 @@ app.post("/api/signup/", checkUsername, function (req, res, next) {
     });
 });
 
-app.get("/api/items/", async function (req, res, next) {
-    const items = await getItems(req.params.page, req.params.limit);
-    return res.json(items);
+app.post('/api/signin/', checkUsername, function (req, res, next) {
+    // extract data from HTTP request
+    if (!('username' in req.body)) return res.status(400).end('username is missing');
+    if (!('password' in req.body)) return res.status(400).end('password is missing');
+    let username = req.body.username;
+    let password = req.body.password;
+    // retrieve user from the database
+    users.findOne({ _id: username }, function (err, user) {
+        if (err) return res.status(500).end(err);
+        if (!user) return res.status(401).end("access denied");
+        compare(password, user.hash, function (err, valid) {
+            if (err) return res.status(500).end(err);
+            if (!valid) return res.status(401).end("access denied");
+            // start a session
+            req.session.user = user;
+            res.setHeader('Set-Cookie', serialize('username', user._id, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 7, // 1 week in number of seconds
+                // secure: true,
+                // sameSite: true
+            }));
+            return res.json(username);
+        });
+    });
 });
 
-app.post("/api/items/", async function (req, res, next) {
-    await addItem(req.body.content);
-    const items = await getItems(req.params.page, req.params.limit);
-    return res.json(items);
+app.get('/api/signout/', function (req, res, next) {
+    req.session.destroy();
+    res.setHeader('Set-Cookie', serialize('username', '', {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 1 week in number of seconds
+        // secure: true,
+        // sameSite: true
+    }));
+    //return res.redirect("/");
+    return res.json({});
 });
 
-app.delete("/api/items/:id/", async function (req, res, next) {
-    await deleteItem(req.params.id);
-    const items = await getItems(req.params.page, req.params.limit);
-    return res.json(items);
+app.get('/api/supported_stock/', isAuthenticated, function (req, res, next) {   //gonna implement caching for this later
+    const url = `${base_path}/stock/symbol?exchange=US&token=cl71pi9r01qvnckae940cl71pi9r01qvnckae94g`
+    fetch(url)
+        .then(x => {
+            if (!x.ok) {
+                return res.status(x.status).end("Finnhub API error")
+            }
+            return res.json(x.json());
+        })
 });
 
-app.use(express.static("static"));
+app.get('/api/company_profile/:symbol/', isAuthenticated, function (req, res, next) {   //gonna implement caching for this later
+    const url = `${base_path}/stock/profile2?symbol=${req.params.symbol}&token=cl71pi9r01qvnckae940cl71pi9r01qvnckae94g`
+    fetch(url)
+        .then(x => {
+            if (!x.ok) {
+                return res.status(x.status).end("Finnhub API call error")
+            }
+            return res.json(x.json());
+        })
+});
+
+app.get('/api/price/:symbol/', isAuthenticated, function (req, res, next) {   //gonna implement caching for this later
+    const url = `${base_path}/quote?symbol=${req.params.symbol}&token=cl71pi9r01qvnckae940cl71pi9r01qvnckae94g`
+    fetch(url)
+        .then(x => {
+            if (!x.ok) {
+                return res.status(x.status).end("Finnhub API call error")
+            }
+            return res.json(x.json());
+        })
+});
+
+// app.get("/api/items/", async function (req, res, next) {
+//     const items = await getItems(req.params.page, req.params.limit);
+//     return res.json(items);
+// });
+
+// app.post("/api/items/", async function (req, res, next) {
+//     await addItem(req.body.content);
+//     const items = await getItems(req.params.page, req.params.limit);
+//     return res.json(items);
+// });
+
+// app.delete("/api/items/:id/", async function (req, res, next) {
+//     await deleteItem(req.params.id);
+//     const items = await getItems(req.params.page, req.params.limit);
+//     return res.json(items);
+// });
+
+
 
 const server = createServer(app).listen(PORT, function (err) {
     if (err) console.log(err);
