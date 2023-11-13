a8c0d651e6e09047943ba63c322ca905734a67ea
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2023-11-12 22:11:06 -0500

    Make config changes

--- fein/client/app/MockData/config.js
+++ fein/client/app/MockData/config.js
@@ -1,5 +1,5 @@
 export const chartConfig = {
-    "1D" : {days: 1, weeks: 0, months: 0, years: 0, resolution: "1"},
+    "3D" : {days: 3, weeks: 0, months: 0, years: 0, resolution: "1"},
     "1W" : {days: 0, weeks: 1, months: 0, years: 0, resolution: "15"},
     "1M" : {days: 0, weeks: 0, months: 1, years: 0, resolution: "60"},
     "1Y" : {days: 0, weeks: 0, months: 0, years: 1, resolution: "D"},
