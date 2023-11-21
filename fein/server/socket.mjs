42ce72188bdd14f927f53f66797b5349abefacda
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2023-11-20 21:01:26 -0500

    Install and set up socket environment

---
+++ fein/server/socket.mjs
@@ -0,0 +1,13 @@
+import { io } from 'socket.io-client';
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
+
+
+export const io = new Server({
+    cors: {
+      origin: "http://localhost:3000"
+    }
+  });
+  
+  io.listen(4000);
